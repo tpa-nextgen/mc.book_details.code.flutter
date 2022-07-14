84019bec9f90b1f22e9db14026b3c03a6d279705
Łukasz Matuszczak <lukasz.matuszczak@appunite.com>
2022-07-14 18:57:27 +0200

    add MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const BookDetailsPage());
+  runApp(const MaterialApp(home: BookDetailsPage()));
 }
 
 class BookDetailsPage extends StatelessWidget {
